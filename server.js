98e62c768206980637486e4b7ac5c73b7664ff51
Steven Lundy <steven.lundy@gmail.com>
2016-01-10 19:21:56 +0000

    Add route to return header info

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('');
+  var ipaddress = req.headers["x-forwarded-for"];
+  var language = req.headers["accept-language"].split(';')[0].split(',')[0];
+  var software = req.headers["user-agent"].match(/\((.+?)\)/)[1];
+  
+  res.send({
+    "ipaddress": ipaddress,
+    "language": language,
+    "software": software
+  });
 });
 
 app.listen(port, function () {
